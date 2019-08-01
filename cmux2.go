146a348165abd488de30abd81817d3e5141ff41e
rbxb <26235793+rbxb@users.noreply.github.com>
2019-08-01 11:51:56 -0700

    Update cmux2.go

--- cmux2.go
+++ cmux2.go
@@ -9,23 +9,13 @@ type Routes map[string]http.HandlerFunc
 
 func Branch(ro Routes) http.HandlerFunc {
 	return func(w http.ResponseWriter, req * http.Request) {
-		v := strings.Split(strings.Trim(req.URL.Path, "/"), "/")
-		if h := match(ro,v); h != nil {
-			req.URL.Path = req.URL.Path[len(v[0]):]
+		p := strings.Trim(req.URL.Path, "/")
+		v := strings.Split(p, "/")
+		if h := ro[v[0]]; h != nil {
+			req.URL.Path = p[len(v[0]):]
 			h(w,req)
 		} else {
 			http.Error(w, "Not found.", 404)
 		}
 	}
-}
-
-func match(ro Routes, v []string) http.HandlerFunc {
-	if len(v) == 0 {
-		if h := ro[""]; h != nil {
-			return h
-		}
-	} else if h := ro[v[0]]; h != nil {
-		return h
-	}
-	return nil
 }
